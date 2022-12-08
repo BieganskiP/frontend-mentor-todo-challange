d861042623bf9364f3ccb46de2b1b0ac3ecaff36
BieganskiP <bieganski1996@gmail.com>
2022-12-08 12:47:40 +0100

    s

--- dist/index.f5c48570.js
+++ dist/index.f5c48570.js
@@ -621,7 +621,247 @@ optionsDesktop.addEventListener("click", (event)=>{
     else return;
 });
 
-},{"notiflix/build/notiflix-notify-aio":"eXQLZ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","../node_modules/uniqid/index":"iN0co"}],"eXQLZ":[function(require,module,exports) {
+},{"../node_modules/uniqid/index":"iN0co","notiflix/build/notiflix-notify-aio":"eXQLZ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"iN0co":[function(require,module,exports) {
+/* 
+(The MIT License)
+Copyright (c) 2014-2021 Halász Ádám <adam@aimform.com>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/ //  Unique Hexatridecimal ID Generator
+// ================================================
+//  Dependencies
+// ================================================
+var process = require("process");
+var pid = typeof process !== "undefined" && process.pid ? process.pid.toString(36) : "";
+var address = "";
+if (typeof __webpack_require__ !== "function" && true) {
+    var mac = "", os = require("os");
+    if (os.networkInterfaces) var networkInterfaces = os.networkInterfaces();
+    if (networkInterfaces) {
+        loop: for(let interface_key in networkInterfaces){
+            const networkInterface = networkInterfaces[interface_key];
+            const length = networkInterface.length;
+            for(var i = 0; i < length; i++)if (networkInterface[i] !== undefined && networkInterface[i].mac && networkInterface[i].mac != "00:00:00:00:00:00") {
+                mac = networkInterface[i].mac;
+                break loop;
+            }
+        }
+        address = mac ? parseInt(mac.replace(/\:|\D+/gi, "")).toString(36) : "";
+    }
+}
+//  Exports
+// ================================================
+module.exports = module.exports.default = function(prefix, suffix) {
+    return (prefix ? prefix : "") + address + pid + now().toString(36) + (suffix ? suffix : "");
+};
+module.exports.process = function(prefix, suffix) {
+    return (prefix ? prefix : "") + pid + now().toString(36) + (suffix ? suffix : "");
+};
+module.exports.time = function(prefix, suffix) {
+    return (prefix ? prefix : "") + now().toString(36) + (suffix ? suffix : "");
+};
+//  Helpers
+// ================================================
+function now() {
+    var time = Date.now();
+    var last = now.last || time;
+    return now.last = time > last ? time : last + 1;
+}
+
+},{"process":"d5jf4","os":"6yyXu"}],"d5jf4":[function(require,module,exports) {
+// shim for using process in browser
+var process = module.exports = {};
+// cached from whatever global is present so that test runners that stub it
+// don't break things.  But we need to wrap it in a try catch in case it is
+// wrapped in strict mode code which doesn't define any globals.  It's inside a
+// function because try/catches deoptimize in certain engines.
+var cachedSetTimeout;
+var cachedClearTimeout;
+function defaultSetTimout() {
+    throw new Error("setTimeout has not been defined");
+}
+function defaultClearTimeout() {
+    throw new Error("clearTimeout has not been defined");
+}
+(function() {
+    try {
+        if (typeof setTimeout === "function") cachedSetTimeout = setTimeout;
+        else cachedSetTimeout = defaultSetTimout;
+    } catch (e) {
+        cachedSetTimeout = defaultSetTimout;
+    }
+    try {
+        if (typeof clearTimeout === "function") cachedClearTimeout = clearTimeout;
+        else cachedClearTimeout = defaultClearTimeout;
+    } catch (e1) {
+        cachedClearTimeout = defaultClearTimeout;
+    }
+})();
+function runTimeout(fun) {
+    if (cachedSetTimeout === setTimeout) //normal enviroments in sane situations
+    return setTimeout(fun, 0);
+    // if setTimeout wasn't available but was latter defined
+    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+        cachedSetTimeout = setTimeout;
+        return setTimeout(fun, 0);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedSetTimeout(fun, 0);
+    } catch (e1) {
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+            return cachedSetTimeout.call(null, fun, 0);
+        } catch (e) {
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+            return cachedSetTimeout.call(this, fun, 0);
+        }
+    }
+}
+function runClearTimeout(marker) {
+    if (cachedClearTimeout === clearTimeout) //normal enviroments in sane situations
+    return clearTimeout(marker);
+    // if clearTimeout wasn't available but was latter defined
+    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+        cachedClearTimeout = clearTimeout;
+        return clearTimeout(marker);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedClearTimeout(marker);
+    } catch (e1) {
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+            return cachedClearTimeout.call(null, marker);
+        } catch (e) {
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+            return cachedClearTimeout.call(this, marker);
+        }
+    }
+}
+var queue = [];
+var draining = false;
+var currentQueue;
+var queueIndex = -1;
+function cleanUpNextTick() {
+    if (!draining || !currentQueue) return;
+    draining = false;
+    if (currentQueue.length) queue = currentQueue.concat(queue);
+    else queueIndex = -1;
+    if (queue.length) drainQueue();
+}
+function drainQueue() {
+    if (draining) return;
+    var timeout = runTimeout(cleanUpNextTick);
+    draining = true;
+    var len = queue.length;
+    while(len){
+        currentQueue = queue;
+        queue = [];
+        while(++queueIndex < len)if (currentQueue) currentQueue[queueIndex].run();
+        queueIndex = -1;
+        len = queue.length;
+    }
+    currentQueue = null;
+    draining = false;
+    runClearTimeout(timeout);
+}
+process.nextTick = function(fun) {
+    var args = new Array(arguments.length - 1);
+    if (arguments.length > 1) for(var i = 1; i < arguments.length; i++)args[i - 1] = arguments[i];
+    queue.push(new Item(fun, args));
+    if (queue.length === 1 && !draining) runTimeout(drainQueue);
+};
+// v8 likes predictible objects
+function Item(fun, array) {
+    this.fun = fun;
+    this.array = array;
+}
+Item.prototype.run = function() {
+    this.fun.apply(null, this.array);
+};
+process.title = "browser";
+process.browser = true;
+process.env = {};
+process.argv = [];
+process.version = ""; // empty string to avoid regexp issues
+process.versions = {};
+function noop() {}
+process.on = noop;
+process.addListener = noop;
+process.once = noop;
+process.off = noop;
+process.removeListener = noop;
+process.removeAllListeners = noop;
+process.emit = noop;
+process.prependListener = noop;
+process.prependOnceListener = noop;
+process.listeners = function(name) {
+    return [];
+};
+process.binding = function(name) {
+    throw new Error("process.binding is not supported");
+};
+process.cwd = function() {
+    return "/";
+};
+process.chdir = function(dir) {
+    throw new Error("process.chdir is not supported");
+};
+process.umask = function() {
+    return 0;
+};
+
+},{}],"6yyXu":[function(require,module,exports) {
+exports.endianness = function() {
+    return "LE";
+};
+exports.hostname = function() {
+    if (typeof location !== "undefined") return location.hostname;
+    else return "";
+};
+exports.loadavg = function() {
+    return [];
+};
+exports.uptime = function() {
+    return 0;
+};
+exports.freemem = function() {
+    return Number.MAX_VALUE;
+};
+exports.totalmem = function() {
+    return Number.MAX_VALUE;
+};
+exports.cpus = function() {
+    return [];
+};
+exports.type = function() {
+    return "Browser";
+};
+exports.release = function() {
+    if (typeof navigator !== "undefined") return navigator.appVersion;
+    return "";
+};
+exports.networkInterfaces = exports.getNetworkInterfaces = function() {
+    return {};
+};
+exports.arch = function() {
+    return "javascript";
+};
+exports.platform = function() {
+    return "browser";
+};
+exports.tmpdir = exports.tmpDir = function() {
+    return "/tmp";
+};
+exports.EOL = "\n";
+exports.homedir = function() {
+    return "/";
+};
+
+},{}],"eXQLZ":[function(require,module,exports) {
 var global = arguments[3];
 /*
 * Notiflix Notify AIO (https://notiflix.github.io)
@@ -1118,246 +1358,6 @@ exports.export = function(dest, destName, get) {
         enumerable: true,
         get: get
     });
-};
-
-},{}],"iN0co":[function(require,module,exports) {
-/* 
-(The MIT License)
-Copyright (c) 2014-2021 Halász Ádám <adam@aimform.com>
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/ //  Unique Hexatridecimal ID Generator
-// ================================================
-//  Dependencies
-// ================================================
-var process = require("process");
-var pid = typeof process !== "undefined" && process.pid ? process.pid.toString(36) : "";
-var address = "";
-if (typeof __webpack_require__ !== "function" && true) {
-    var mac = "", os = require("os");
-    if (os.networkInterfaces) var networkInterfaces = os.networkInterfaces();
-    if (networkInterfaces) {
-        loop: for(let interface_key in networkInterfaces){
-            const networkInterface = networkInterfaces[interface_key];
-            const length = networkInterface.length;
-            for(var i = 0; i < length; i++)if (networkInterface[i] !== undefined && networkInterface[i].mac && networkInterface[i].mac != "00:00:00:00:00:00") {
-                mac = networkInterface[i].mac;
-                break loop;
-            }
-        }
-        address = mac ? parseInt(mac.replace(/\:|\D+/gi, "")).toString(36) : "";
-    }
-}
-//  Exports
-// ================================================
-module.exports = module.exports.default = function(prefix, suffix) {
-    return (prefix ? prefix : "") + address + pid + now().toString(36) + (suffix ? suffix : "");
-};
-module.exports.process = function(prefix, suffix) {
-    return (prefix ? prefix : "") + pid + now().toString(36) + (suffix ? suffix : "");
-};
-module.exports.time = function(prefix, suffix) {
-    return (prefix ? prefix : "") + now().toString(36) + (suffix ? suffix : "");
-};
-//  Helpers
-// ================================================
-function now() {
-    var time = Date.now();
-    var last = now.last || time;
-    return now.last = time > last ? time : last + 1;
-}
-
-},{"process":"d5jf4","os":"6yyXu"}],"d5jf4":[function(require,module,exports) {
-// shim for using process in browser
-var process = module.exports = {};
-// cached from whatever global is present so that test runners that stub it
-// don't break things.  But we need to wrap it in a try catch in case it is
-// wrapped in strict mode code which doesn't define any globals.  It's inside a
-// function because try/catches deoptimize in certain engines.
-var cachedSetTimeout;
-var cachedClearTimeout;
-function defaultSetTimout() {
-    throw new Error("setTimeout has not been defined");
-}
-function defaultClearTimeout() {
-    throw new Error("clearTimeout has not been defined");
-}
-(function() {
-    try {
-        if (typeof setTimeout === "function") cachedSetTimeout = setTimeout;
-        else cachedSetTimeout = defaultSetTimout;
-    } catch (e) {
-        cachedSetTimeout = defaultSetTimout;
-    }
-    try {
-        if (typeof clearTimeout === "function") cachedClearTimeout = clearTimeout;
-        else cachedClearTimeout = defaultClearTimeout;
-    } catch (e1) {
-        cachedClearTimeout = defaultClearTimeout;
-    }
-})();
-function runTimeout(fun) {
-    if (cachedSetTimeout === setTimeout) //normal enviroments in sane situations
-    return setTimeout(fun, 0);
-    // if setTimeout wasn't available but was latter defined
-    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
-        cachedSetTimeout = setTimeout;
-        return setTimeout(fun, 0);
-    }
-    try {
-        // when when somebody has screwed with setTimeout but no I.E. maddness
-        return cachedSetTimeout(fun, 0);
-    } catch (e1) {
-        try {
-            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
-            return cachedSetTimeout.call(null, fun, 0);
-        } catch (e) {
-            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
-            return cachedSetTimeout.call(this, fun, 0);
-        }
-    }
-}
-function runClearTimeout(marker) {
-    if (cachedClearTimeout === clearTimeout) //normal enviroments in sane situations
-    return clearTimeout(marker);
-    // if clearTimeout wasn't available but was latter defined
-    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
-        cachedClearTimeout = clearTimeout;
-        return clearTimeout(marker);
-    }
-    try {
-        // when when somebody has screwed with setTimeout but no I.E. maddness
-        return cachedClearTimeout(marker);
-    } catch (e1) {
-        try {
-            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
-            return cachedClearTimeout.call(null, marker);
-        } catch (e) {
-            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
-            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
-            return cachedClearTimeout.call(this, marker);
-        }
-    }
-}
-var queue = [];
-var draining = false;
-var currentQueue;
-var queueIndex = -1;
-function cleanUpNextTick() {
-    if (!draining || !currentQueue) return;
-    draining = false;
-    if (currentQueue.length) queue = currentQueue.concat(queue);
-    else queueIndex = -1;
-    if (queue.length) drainQueue();
-}
-function drainQueue() {
-    if (draining) return;
-    var timeout = runTimeout(cleanUpNextTick);
-    draining = true;
-    var len = queue.length;
-    while(len){
-        currentQueue = queue;
-        queue = [];
-        while(++queueIndex < len)if (currentQueue) currentQueue[queueIndex].run();
-        queueIndex = -1;
-        len = queue.length;
-    }
-    currentQueue = null;
-    draining = false;
-    runClearTimeout(timeout);
-}
-process.nextTick = function(fun) {
-    var args = new Array(arguments.length - 1);
-    if (arguments.length > 1) for(var i = 1; i < arguments.length; i++)args[i - 1] = arguments[i];
-    queue.push(new Item(fun, args));
-    if (queue.length === 1 && !draining) runTimeout(drainQueue);
-};
-// v8 likes predictible objects
-function Item(fun, array) {
-    this.fun = fun;
-    this.array = array;
-}
-Item.prototype.run = function() {
-    this.fun.apply(null, this.array);
-};
-process.title = "browser";
-process.browser = true;
-process.env = {};
-process.argv = [];
-process.version = ""; // empty string to avoid regexp issues
-process.versions = {};
-function noop() {}
-process.on = noop;
-process.addListener = noop;
-process.once = noop;
-process.off = noop;
-process.removeListener = noop;
-process.removeAllListeners = noop;
-process.emit = noop;
-process.prependListener = noop;
-process.prependOnceListener = noop;
-process.listeners = function(name) {
-    return [];
-};
-process.binding = function(name) {
-    throw new Error("process.binding is not supported");
-};
-process.cwd = function() {
-    return "/";
-};
-process.chdir = function(dir) {
-    throw new Error("process.chdir is not supported");
-};
-process.umask = function() {
-    return 0;
-};
-
-},{}],"6yyXu":[function(require,module,exports) {
-exports.endianness = function() {
-    return "LE";
-};
-exports.hostname = function() {
-    if (typeof location !== "undefined") return location.hostname;
-    else return "";
-};
-exports.loadavg = function() {
-    return [];
-};
-exports.uptime = function() {
-    return 0;
-};
-exports.freemem = function() {
-    return Number.MAX_VALUE;
-};
-exports.totalmem = function() {
-    return Number.MAX_VALUE;
-};
-exports.cpus = function() {
-    return [];
-};
-exports.type = function() {
-    return "Browser";
-};
-exports.release = function() {
-    if (typeof navigator !== "undefined") return navigator.appVersion;
-    return "";
-};
-exports.networkInterfaces = exports.getNetworkInterfaces = function() {
-    return {};
-};
-exports.arch = function() {
-    return "javascript";
-};
-exports.platform = function() {
-    return "browser";
-};
-exports.tmpdir = exports.tmpDir = function() {
-    return "/tmp";
-};
-exports.EOL = "\n";
-exports.homedir = function() {
-    return "/";
 };
 
 },{}]},["4qavH","8ZNvh"], "8ZNvh", "parcelRequire716c")
